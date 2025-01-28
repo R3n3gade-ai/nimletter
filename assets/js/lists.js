6ee528536e40a65b598e63fd17c11036f047ee47
ThomasTJdev <ttj@ttj.dk>
2025-01-28 21:56:51 +0100

    JS avoid clash

--- assets/js/lists.js
+++ assets/js/lists.js
@@ -351,7 +351,7 @@ function openList(listID) {
                   jsCreateElement('button', {
                     attrs: {
                       class: 'svg16',
-                      onclick: `removeFlow(${flow.id})`
+                      onclick: `removeFromFromList(${flow.id})`
                     },
                     rawHtml: [
                       '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/></svg>'
@@ -418,7 +418,7 @@ function updateList(listID) {
   });
 }
 
-function removeFlow(flowID) {
+function removeFromFromList(flowID) {
   const html = jsCreateElement('div', {
     attrs: {
       style: "width: 300px;"
@@ -443,7 +443,7 @@ function removeFlow(flowID) {
           jsCreateElement('button', {
             attrs: {
               class: 'buttonIcon',
-              onclick: 'removeFlowDo(' + flowID + ')'
+              onclick: 'removeFlowFromListDo(' + flowID + ')'
             },
             rawHtml: [
               '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m-12 .562a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"></path></svg><div style="margin-left: 5px;">Remove</div>'
@@ -456,7 +456,7 @@ function removeFlow(flowID) {
   rawModalLoader(jsRender(html));
 }
 
-function removeFlowDo(flowID) {
+function removeFlowFromListDo(flowID) {
   // Implement the function to remove a flow from the list
   fetch("/api/lists/flow/remove", {
     method: "POST",
